9455223f14f8e6973b99cce0ed3f6e0c8565df1c
A.Amani <didi1364@gmail.com>
2022-07-09 00:11:33 +0430

    adc ok tested with function generator

--- src/main.c
+++ src/main.c
@@ -6,12 +6,10 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "sdkconfig.h"
-
 #include "driver/uart.h"
-#include "driver/gpio.h"
-#include "sdkconfig.h"
+#include "driver/adc.h"
 #include "esp_log.h"
-
+#include "esp_adc_cal.h"
 #define BLINK_GPIO 2
 #define BUF_SIZE (1024)
 #define ECHO_UART_PORT_NUM 0
@@ -21,6 +19,32 @@
 #define ECHO_TEST_CTS 19
 #define ReverseUInt(value) \
 ((value >> 24) | ((value >> 16) << 8) | ((value & 0xff00) << 8) | ((value& 0xff) << 24))
+
+#define ADC1_EXAMPLE_CHAN0          ADC1_CHANNEL_6
+#define ADC_EXAMPLE_CALI_SCHEME     ESP_ADC_CAL_VAL_EFUSE_VREF
+#define ADC_EXAMPLE_ATTEN           ADC_ATTEN_DB_11
+static esp_adc_cal_characteristics_t adc1_chars;
+uint32_t  voltage=0;
+static bool adc_calibration_init(void)
+{
+    esp_err_t ret;
+    bool cali_enable = false;
+ printf("adc Init...\n");
+    ret = esp_adc_cal_check_efuse(ADC_EXAMPLE_CALI_SCHEME);
+    if (ret == ESP_ERR_NOT_SUPPORTED) {
+        printf( "Calibration scheme not supported, skip software calibration\n");
+    } else if (ret == ESP_ERR_INVALID_VERSION) {
+        printf("eFuse not burnt, skip software calibration\n");
+    } else if (ret == ESP_OK) {
+        cali_enable = true;
+        esp_adc_cal_characterize(ADC_UNIT_1, ADC_EXAMPLE_ATTEN, ADC_WIDTH_BIT_DEFAULT, 0, &adc1_chars);
+ printf("adc Init ok\n");
+    } else {
+        printf("Invalid arg\n");
+    }
+
+    return cali_enable;
+}
 
 //====================================================================================================================
 void UARTInit()
@@ -46,13 +70,6 @@ void UARTInit()
      ESP_ERROR_CHECK(uart_param_config(ECHO_UART_PORT_NUM, &uart_config));
      ESP_ERROR_CHECK(uart_set_pin(ECHO_UART_PORT_NUM, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS));
 
-}
-//====================================================================================================================
-void AppendToArray(uint8_t data,uint8_t *array)
-{
-*array<<=8;
-*array=data;
-  
 }
 
 uint32_t crc32(unsigned char *message, int len)
@@ -107,18 +124,25 @@ void app_main()
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
     UARTInit();
+    adc_calibration_init();
+    ESP_ERROR_CHECK(adc1_config_width(ADC_WIDTH_BIT_DEFAULT));
+    ESP_ERROR_CHECK(adc1_config_channel_atten(ADC1_EXAMPLE_CHAN0, ADC_EXAMPLE_ATTEN));
     while(1) {
         /* Blink off (output low) */
 	// printf("Turning off the LED\n");
+    voltage = esp_adc_cal_raw_to_voltage(adc1_get_raw(ADC1_EXAMPLE_CHAN0), &adc1_chars);
+    printf("v= %d mv\n",voltage);
+
+
          gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
         /* Blink on (output high) */
-        SendTemp(22);
+        // SendTemp(22);
 	// printf("Turning on the LED\n");
         gpio_set_level(BLINK_GPIO, 1);
      
       
 
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     }
 }
